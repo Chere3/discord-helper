b9aa85521368f8ab6091db1e0cf59a314cddaab8
Cheree <71246795+Chere3@users.noreply.github.com>
2023-09-10 18:54:13 -0600

    build: add sheet version

--- src/config.ts
+++ src/config.ts
@@ -15,9 +15,10 @@ const defaultConfig = {
     ai: {
         systemMessage: `TASK: 
         You are a bot that answers questions related to programming. You have to answer the questions as if you were a human developer. 
-        But you have to do it in a way that is not too technical if the question is not too technical. 
+        But you have to do it in a way that is not too technical if the question is not too technical.
         You have to use a format to make an order. The format is:
-        Title score based on the question ("title" input in user format), and if score is lower than 5, make a new title that match the question: number (1 - 10) - Possible title if score is lower than 5, if the title does not describe the question, make a the score lower.
+        
+        Title: Based in the question title, how much the question is related to the question or problem. (0-10) If the question is not related to the problem, the score is 0. (The score is based in the title) - (Suggested title in spanish if the score is less than 5)
         Response: The response to the question
 
         EXAMPLE RESPONSE:
@@ -25,12 +26,21 @@ const defaultConfig = {
         Response: You can use console.log() to print something in the console in JavaScript.
 
         QUESTION: How to print something in the console in JavaScript?
+        USER TITLE: How javscript prints console
 
         EXAMPLE RESPONSE 2:
         Title: 3 - How to print something in the console in JavaScript?
         Response: You can use console.log() to print something in the console in JavaScript.
 
         QUESTION: How javscript prints console
+        USER TITLE: js console
+
+        EXAMPLE RESPONSE 3:
+        Title: 0 - Como imprimir en la consola en JavaScript? (por que la pregunta no tiene que ver con el título)
+        Response: Puedes usar console.log() para imprimir algo en la consola en JavaScript.
+
+        QUESTION: Como imprimir en la consola en JavaScript?
+        USER TITLE: impimir
         `,
     },
 };
--- src/events/helpThreads/helpThreadCreate.ts
+++ src/events/helpThreads/helpThreadCreate.ts
@@ -4,45 +4,42 @@ import { PyEGPT } from '../../modules/client';
 const run = async (client: PyEGPT, channel: ThreadChannel, parentChannel: TextChannel) => {
     await channel.sendTyping();
 
-    const message = await channel.fetchStarterMessage();
-    if (!message) return;
+    try {
+        const message = await channel.fetchStarterMessage();
+        if (!message) return;
 
-    const ai_message = await client.ai.createMessage([
-        { role: 'system', content: client.config.ai.systemMessage },
-        {
-            role: 'user',
-            content: `Title: ${channel.name}\nProblem: ${message.content}`,
-            name: message.author.globalName ?? message.author.tag,
-        },
-    ]);
-
-    if (!ai_message || !ai_message.choices[0].message) return;
-
-    // match the título or title
-    const match = ai_message.choices[0].message.content?.match(/(T[ií]tulo:|Title:).*/g);
-    console.log(match);
-
-    const score = match?.[0].match(/\d+/)?.[0];
-    const title = match?.[0]?.toString().split('-')[1];
-    console.log(title);
-
-    console.log(score);
-
-    if ((match || score || title) && parseInt(score ?? '0') <= 5)
-        await channel.setName(`${title?.slice(0, 97) ?? ''}...`);
-
-    await message.reply({
-        embeds: [
+        const ai_message = await client.ai.createMessage([
+            { role: 'system', content: client.config.ai.systemMessage },
             {
-                color: Colors.Blue,
-                description: `${
-                    ai_message?.choices[0].message.content
-                        ?.replaceAll(match?.toString() ?? ':v', '')
-                        .replaceAll(/(Respuesta:|Response:) .*/g, '') ?? 'No se ha podido crear el mensaje.'
-                }`,
+                role: 'user',
+                content: `USER TITLE: ${channel.name}\nQUESTION: ${message.content}`,
+                name: message.author.globalName ?? message.author.tag,
             },
-        ],
-    });
+        ]);
+
+        if (!ai_message || !ai_message.choices[0].message) return;
+
+        const match = ai_message.choices[0].message.content?.match(/(T[ií]tulo:|Title:).*/g);
+        const score = match?.[0].match(/\d+/)?.[0];
+        const title = match?.[0]?.toString().split('-')[1];
+
+        if ((match || score || title) && parseInt(score ?? '0') <= 5)
+            await channel.setName(`${title?.slice(0, 97) ?? ''}...`);
+
+        await message.reply({
+            embeds: [
+                {
+                    color: Colors.Blue,
+                    description: `${
+                        ai_message?.choices[0].message.content?.replace(match?.toString() ?? '__ds', '') ??
+                        'No se ha podido crear el mensaje.'
+                    }`,
+                },
+            ],
+        });
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 export default run;
